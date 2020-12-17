5ba4a7398b4a08b3189e377148ce0b0e077d905f
chongqianqian <yunle@dtstack.com>
2020-12-17 11:20:53 +0800

    修复路由问题

--- src/router/index.js
+++ src/router/index.js
@@ -80,3 +80,7 @@ export default new Router({
     }
   ]
 })
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+return originalPush.call(this, location).catch(err => err)
+}
